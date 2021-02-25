764808bf732e77028fd2a43e5540626679eef831
rasmus98 <37410554+rasmus98@users.noreply.github.com>
2021-02-25 17:36:33 +0100

    Update rasmus.py
    Add a third approach: using numba. Faster for both small and large inputs

--- rasmus.py
+++ rasmus.py
@@ -1,11 +1,33 @@
 import numpy as np
 
-class rasmus_fast:
+class rasmus_simple:
     def __init__(self): self.name = "Rasmus' fast implementation"
         
     def Z(self, n, w):
         # Much faster than native python loop
         return 2*np.prod(np.exp(omega)+np.exp(-omega))
+    
+import numba as nb
+class rasmus_numba:
+    def __init__(self): 
+        self.name = "Rasmus' fast implementation"
+        self.par_Z(np.random.randn(100)) # Init JIT for serial
+        self.ser_Z(np.random.randn(1000)) # Init JIT for parallel
+    
+    def Z(self, n, w):
+        # Depending on size, serial or parallel may be best. 
+        # crossover point found by profiling
+        return (self.ser_Z if (len(w) < 375) else self.par_Z)(w)
+       
+    @staticmethod
+    @nb.njit("f8(f8[:])", fastmath=True, parallel=True)
+    def par_Z(w):
+        return 2*np.prod(2*np.cosh(w))
+    
+    @staticmethod
+    @nb.njit("f8(f8[:])", fastmath=True, parallel=False)
+    def ser_Z(w):
+        return 2*np.prod(2*np.cosh(w))
     
 class rasmus_brute:
     def __init__(self): self.name = "Rasmus' vectorized brute implementation"
